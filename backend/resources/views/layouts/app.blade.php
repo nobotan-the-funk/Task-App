3a119a91d84f057c5b254c81600a7a98449aaff5
nobotan-the-funk <nobotan.the.funk@gmail.com>
2021-09-25 20:56:34 +0900

    編集

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -12,12 +12,10 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
   <!-- Styles -->
-  <!-- ↓変更 -->
-  <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{asset('css/app.css') }}">
 
   <!-- Scripts -->
-  <!-- ↓変更 -->
-  <script src="{{ secure_asset('js/app.js') }}" defer></script>
+  <script src="{{asset('js/app.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">
